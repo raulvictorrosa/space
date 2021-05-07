2ded01170c68160d8939387bddf9cd911eb43d43
Raul Rosa <raulvictorrosa@gmail.com>
2021-05-07 17:49:07 -0300

    feat: import scss on _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import '../styles/app.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
 
-export default MyApp;
+export default App;
