ba96f2251b254a8eb9a1a216074f13ae4a81671a
Raul Rosa <raulvictorrosa@gmail.com>
2021-05-08 20:38:51 -0300

    feat: fake data to call on components

---
+++ fakeData.tsx
@@ -0,0 +1,65 @@
+export const captionTest =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie elit at lacus…';
+
+export const newsTest = [
+  {
+    id: 'test1',
+    title:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing ligula eget dolor.'
+  },
+  {
+    id: 'test2',
+    title:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing ligula eget dolor.'
+  },
+  {
+    id: 'test3',
+    title:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing ligula eget dolor.'
+  }
+];
+
+export const carousels = [
+  {
+    id: 'post-1',
+    title: 'International Space Station',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur luctus aliquet sapien….',
+    image: { id: 1, path: '/images/image2.jpg' }
+  },
+  {
+    id: 'post-2',
+    title: 'My capsule',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur luctus aliquet sapien….',
+    image: { id: 2, path: '/images/image1.jpg' }
+  },
+  {
+    id: 'post-3',
+    title: 'My moon',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur luctus aliquet sapien….',
+    image: { id: 3, path: '/images/image3.jpg' }
+  },
+  {
+    id: 'post-4',
+    title: 'International Space Station',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur luctus aliquet sapien….',
+    image: { id: 4, path: '/images/image2.jpg' }
+  },
+  {
+    id: 'post-5',
+    title: 'My capsule',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur luctus aliquet sapien….',
+    image: { id: 5, path: '/images/image1.jpg' }
+  },
+  {
+    id: 'post-6',
+    title: 'My moon',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur luctus aliquet sapien….',
+    image: { id: 6, path: '/images/image3.jpg' }
+  }
+];
