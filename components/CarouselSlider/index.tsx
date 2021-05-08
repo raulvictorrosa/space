a7bd7f42b8d893986f21f4b0d7789184cd90ed5f
Raul Rosa <raulvictorrosa@gmail.com>
2021-05-08 20:35:03 -0300

    feat: Carousel Slider component

---
+++ components/CarouselSlider/index.tsx
@@ -0,0 +1,88 @@
+import {
+  ButtonBack,
+  ButtonNext,
+  CarouselProvider,
+  Image,
+  Slide,
+  Slider
+} from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import React from 'react';
+import { Card, CardBody, CardText, CardTitle, Col, Row } from 'reactstrap';
+import { CarouselContainer, CarouselIcon } from './styled';
+
+type ItemType = {
+  id: string;
+  title: string;
+  description: string;
+  image: {
+    id: number;
+    path: string;
+  };
+};
+
+type CardItemType = React.PropsWithChildren<{ item: ItemType }>;
+
+type CarouselSliderType = React.FC<{
+  data: ItemType[];
+  isPlaying: boolean;
+  infinite?: boolean;
+}>;
+
+const CardItem: React.FC<CardItemType> = ({
+  item: { title, description, image }
+}) => (
+  <Card>
+    <Image
+      hasMasterSpinner
+      isBgImage
+      tag="div"
+      className="card-img-top"
+      src={image.path}
+    />
+    <CardBody>
+      <CardTitle tag="h5">{title}</CardTitle>
+      <CardText>{description}</CardText>
+    </CardBody>
+  </Card>
+);
+
+const CarouselSlider: CarouselSliderType = ({
+  data,
+  isPlaying,
+  infinite = false
+}) => {
+  return (
+    <CarouselContainer className="pl-0 pr-0">
+      <Row>
+        <Col>
+          <CarouselProvider
+            visibleSlides={3}
+            totalSlides={data.length}
+            naturalSlideWidth={368}
+            naturalSlideHeight={462}
+            hasMasterSpinner
+            isPlaying={isPlaying}
+            infinite={infinite}
+          >
+            <Slider>
+              {data.map((item: ItemType, i: number) => (
+                <Slide index={i}>
+                  <CardItem item={item} />
+                </Slide>
+              ))}
+            </Slider>
+            <ButtonBack className="carousel-button">
+              <CarouselIcon aria-hidden="true" data-icon="&#x34;" />
+            </ButtonBack>
+            <ButtonNext className="carousel-button">
+              <CarouselIcon aria-hidden="true" data-icon="&#x35;" />
+            </ButtonNext>
+          </CarouselProvider>
+        </Col>
+      </Row>
+    </CarouselContainer>
+  );
+};
+
+export default CarouselSlider;
---
+++ components/CarouselSlider/styled.tsx
@@ -0,0 +1,60 @@
+import { Container } from 'reactstrap';
+import styled from 'styled-components';
+
+const CardBorderRadius = '8px';
+
+export const CarouselContainer = styled(Container)`
+  .carousel {
+    margin-left: -32px;
+
+    &__image {
+      background-position: center;
+    }
+    &-button {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      border: 0;
+      background: none;
+      padding: 0;
+      margin-top: -22px;
+    }
+    &__back-button {
+      left: -28px;
+    }
+    &__next-button {
+      right: 0;
+    }
+  }
+
+  .card {
+    max-width: 368px;
+    margin-left: 32px;
+    border: 0;
+    border-radius: ${CardBorderRadius};
+
+    &-body {
+      padding: 1.45rem 1.55rem;
+    }
+    &-img-top {
+      border-top-left-radius: ${CardBorderRadius};
+      border-top-right-radius: ${CardBorderRadius};
+      height: 309px;
+    }
+    &-title {
+      color: #3e4d60;
+      line-height: 30px;
+    }
+  }
+`;
+
+export const CarouselIcon = styled.div`
+  &[data-icon]:before {
+    display: block;
+    color: #4a90e2;
+    font-size: 72px;
+    line-height: 32px;
+    width: 16.5px;
+    height: 32px;
+  }
+`;
