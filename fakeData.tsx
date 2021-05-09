5c891431b9d71c355c280fad278b83d9305eceec
Raul Rosa <raulvictorrosa@gmail.com>
2021-05-08 23:43:22 -0300

    feat: fake data post about

--- fakeData.tsx
+++ fakeData.tsx
@@ -63,3 +63,13 @@ export const carousels = [
     image: { id: 6, path: '/images/image3.jpg' }
   }
 ];
+
+export const aboutData = [
+  {
+    id: 'post-112',
+    title: 'About Us',
+    abstract: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.\n
+      In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.`,
+    image: { id: 1, path: '/images/image1.jpg' }
+  }
+];
